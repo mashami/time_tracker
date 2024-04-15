0c164f8c267daf42923efc8d9f9ad2cda180e81f
Maker Studios <mashamipaccy04@gmail.com>
2024-04-15 11:51:05 +0200

    feat:creating sign up for user api

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,3 +2,18 @@ export interface SigninType {
   email: string
   password: string
 }
+
+export interface registerType {
+  name: string
+  email: string
+  password: string
+  retypedPassword: string
+}
+
+export interface SignupType {
+  name: string
+  email: string
+  password: string
+  retypePassword: string
+  companyId: string
+}
